e157a6083fdbffd19eb9e314f024c6abeb2ef6a4
habib-e <basharmd978@gmail.com>
2023-12-04 11:57:35 +0600

    First version

--- First-Group-Project/react-todo-app/src/pages/Profile/profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { MyProfile } from '../../components/MyProfile/myProfile'
-
-export const ProfilePage = () => {
-  return (
-
-    <MyProfile></MyProfile>
-  )
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Learnathon2.0
