6cba8a1168f77104b7fd6d7e80f8e00a52828c19
habib-e <basharmd978@gmail.com>
2023-12-04 13:18:23 +0600

    delete first version

---
+++ portfolio-react-app/src/data/data.js
@@ -0,0 +1,35 @@
+import RealEst from '../assets/projects/realestate.jpg'
+import WorkIm from '../assets/projects/workImg.jpeg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"https://github.com/morshedulmunna/Codemind---Hackathon-2023",
+        live:"https://celadon-hub.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Flutter Application",
+        image:RealEst,
+        github:"https://github.com/habib-e/Tutor-Media-Using-Flutter",
+        live:"",
+    },
+    {
+        id:3,
+        name:"JS Application",
+        image:WorkIm,
+        github:"https://github.com/habib-e/Mobile-shop-ecommerse-website",
+        live:"",
+    },
+    {
+        id:4,
+        name:"C# DotNet Desktop Application",
+        image:RealEst,
+        github:"https://github.com/habib-e/laptopmanagementshop",
+        live:"",
+    },
+
+
+]
