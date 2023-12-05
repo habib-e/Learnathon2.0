580cc6e74c7eb7ec65fe8d34d93d7fdc889d88e3
habib-e <basharmd978@gmail.com>
2023-12-05 18:51:05 +0600

    main jsx add

---
+++ First-Group-Project/react-todo-app/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { Auth0Provider } from '@auth0/auth0-react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Auth0Provider
+		domain="dev-habib.us.auth0.com"
+		clientId="KfVNQlVQ9f8twUEbTsXyJiU6PePULx3J"
+		authorizationParams={{
+			redirect_uri: window.location.origin
+		}}
+	>
+		<App />
+	</Auth0Provider>
+)
